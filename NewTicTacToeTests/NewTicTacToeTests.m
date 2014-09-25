0454060edb3ba72d554962fc42005d30b26b3561
Albert Saucedo <saucedoa7@gmail.com>
2014-09-24 21:54:52 -0500

    Initial Commit

---
+++ NewTicTacToeTests/NewTicTacToeTests.m
@@ -0,0 +1,34 @@
+//
+//  NewTicTacToeTests.m
+//  NewTicTacToeTests
+//
+//  Created by Albert Saucedo on 9/24/14.
+//  Copyright (c) 2014 Albert Saucedo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NewTicTacToeTests : XCTestCase
+
+@end
+
+@implementation NewTicTacToeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
